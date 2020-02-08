485562866fdf9f0c9fc1a4f712826a9fb1ccb127
Airton Toyansk <atoyansk@gmail.com>
2020-02-08 17:44:30 +0200

    First: Components structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './web/home/home.component';
+import { SkillsComponent } from './web/skills/skills.component';
+import { AboutComponent } from './web/about/about.component';
+import { LoginComponent } from './admin/login/login.component';
+import { PortfolioComponent } from './admin/portfolio/portfolio.component';
+import { MessagesComponent } from './admin/messages/messages.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SkillsComponent,
+    AboutComponent,
+    LoginComponent,
+    PortfolioComponent,
+    MessagesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
