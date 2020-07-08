1bbfc0be10c407d94a75778af4283997e13a1562
Airton Toyansk <atoyansk@gmail.com>
2020-07-08 22:50:17 +0300

    Dynamic charts from Firestore

---
+++ src/app/models/skills.model.ts
@@ -0,0 +1,5 @@
+export class Skills {
+    key: string;
+    name: string;
+    percentage: string;
+}
--- src/app/web/about/about.component.ts
+++ src/app/web/about/about.component.ts
@@ -3,6 +3,10 @@ import { DOCUMENT } from '@angular/common';
 import { ScrollnavService } from '../../services/scrollnav.service';
 import { CrudMethodsService } from '../../services/crud-methods.service';
 import { About } from '../../models/about.model';
+import { Skills } from '../../models/skills.model';
+import * as pluginDataLabels from 'chartjs-plugin-datalabels';
+import { Label } from 'ng2-charts';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-about',
@@ -15,38 +19,18 @@ export class AboutComponent implements OnInit {
   basePath = 'about';
   about: About[];
 
-  barChartOptions = {
-    scaleShowVerticalLines: false,
-    responsive: true,
-    scales: {
-      xAxes: [{
-        ticks: {
-          beginAtZero: true
-        },
-        gridLines: {
-            display: false
-        }
-      }],
-      yAxes: [{
-        gridLines: {
-            display: false
-        }
-      }]
-    }
-  };
-  barChartLabels = ['2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  chartColors: Array<any> = [{ backgroundColor: '#27dfff'}];
-  barChartType = 'horizontalBar';
-  barChartLegend = true;
-  barChartData = [
-    {
-      data: [10, 65, 59, 80, 81, 56, 55, 100],
-      label: 'My Skills',
-      barThickness: 15,
-      maxBarThickness: 20,
-      minBarLength: 2
-    }
-  ];
+  basePath2 = 'skills';
+  skills;
+
+  barChartOptions = {};
+  barChartLabels = [];
+  bar = [];
+  valores = [];
+  chartColors = [];
+  barChartType;
+  barChartLegend;
+  barChartPlugins = [];
+  barChartData = [{}];
 
   constructor(@Inject(DOCUMENT) private document: Document,
               private scrollserv: ScrollnavService,
@@ -82,6 +66,75 @@ export class AboutComponent implements OnInit {
         return data;
       });
     });
+
+    this.skills = this.crudService.getItems(this.basePath2).pipe(
+      map (graph => graph.map(a => {
+      const data = a.payload.doc.data() as Skills;
+      const id = a.payload.doc.id;
+      return { id, ...data };
+      })
+    ));
+
+    this.skills.subscribe(item => {
+      item.forEach(i => {
+        this.bar.push(i.name);
+        this.valores.push(i.percentage);
+      });
+    });
+
+    this.barChartOptions = {
+      scaleShowVerticalLines: false,
+      responsive: true,
+      maintainAspectRatio: false,
+      tooltips: {
+        enabled: false
+      },
+      plugins: {
+        datalabels: {
+          anchor: 'end',
+          align: 'start',
+          offset: 10,
+          font: {
+            weight: 'bold',
+            size: '15'
+          },
+          color: 'white'
+        }
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            beginAtZero: true,
+            display: false
+          },
+          gridLines: {
+              display: false
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            fontSize: 15
+          },
+          gridLines: {
+              display: false
+          }
+        }]
+      }
+    };
+    this.barChartLabels = this.bar;
+    this.chartColors = [{ backgroundColor: '#27dfff'}];
+    this.barChartType = 'horizontalBar';
+    this.barChartLegend = false;
+    this.barChartPlugins = [pluginDataLabels];
+    this.barChartData = [
+      {
+        data: this.valores,
+        label: 'My Skills',
+        barThickness: 18,
+        maxBarThickness: 20,
+        minBarLength: 2
+      }
+    ];
   }
 
 }
